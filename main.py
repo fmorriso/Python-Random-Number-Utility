a34f6b783a95fbd69d6c1e4b21c9813c9602ff9d
Fred Morrison <frederick.morrison@outlook.com>
2025-04-20 11:28:09 -0600

    adjust verification tests

--- main.py
+++ main.py
@@ -4,15 +4,36 @@ from random_number_utilities import RandomNumberUtilities
 
 
 def verify_random_int_works():
-    for test in range(100):
-        nums = RandomNumberUtilities.random_int(1, 6, 20)
-        print(f'nums: {nums}')
+    min_value: int = 1
+    max_value: int = 6
 
-
+    max_found: int = 0
+    min_found: int = sys.maxsize
     # keep track of how many times the min_value and max_value were generated
     frequency = {'min': 0, 'max': 0}
+    for test in range(1_000):
+        nums = RandomNumberUtilities.random_int(min_value, max_value, 20)
+        # print(f'nums: {nums}')
+
+        max_found = max(max_found, nums.max())
+        min_found = min(min_found, nums.min())
+
+        min_matches = nums[nums == min_value]
+        if len(min_matches) > 0:
+            frequency['min'] += len(min_matches)
+
+        max_matches = nums[nums == max_value]
+        if len(max_matches) > 0:
+            frequency['max'] += len(max_matches)
+
+    print(f'min_found: {min_found}, max_found: {max_found}')
+    print(f'{frequency = }')
+
 
 def verify_random_float_with_tolerance_works():
+    min_value: float = 1.0
+    max_value: float = 6.0
+
     max_found: float = 0
     min_found: float = sys.float_info.max
     # keep track of how many times the min_value and max_value were generated
@@ -22,16 +43,14 @@ def verify_random_float_with_tolerance_works():
         # print(f'{test} nums: {nums}')
         max_found = max(max_found, nums.max())
         min_found = min(min_found, nums.min())
-        filtered_max_values = nums[(nums >= 5.999) & (nums <= 6.001)]
-        filtered_min_values = nums[(nums >= 0.999) & (nums <= 1.001)]
-        # print(f'filtered_values: {filtered_values}')
-        if len(filtered_max_values) > 0:
-            #print(f'max_found: {max_found} in test {test}')
-            frequency['max'] += 1
 
-        if len(filtered_min_values) > 0:
-            #print(f'min_found: {min_found} in test {test}')
-            frequency['min'] += 1
+        min_matches = nums[nums == min_value]
+        if len(min_matches) > 0:
+            frequency['min'] += len(min_matches)
+
+        max_matches = nums[nums == max_value]
+        if len(max_matches) > 0:
+            frequency['max'] += len(max_matches)
 
 
     print(f'min_found: {min_found}, max_found: {max_found}')

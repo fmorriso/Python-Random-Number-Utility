061eb3a515aa0d86a9b6b2c72c1a9a78caf0d21c
Fred Morrison <frederick.morrison@outlook.com>
2025-04-20 10:35:51 -0600

    random_float_with_tolerance is working

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
+import sys
+
 from random_number_utilities import RandomNumberUtilities
 
 
-def main():
+def verify_random_int_works():
     nums = RandomNumberUtilities.random_int(1, 6, 20)
     print(f'nums: {nums}')
+
+def verify_random_float_works():
+    max_found = 0
+    min_found = sys.float_info.max
+    for test in range(100):
+        nums = RandomNumberUtilities.random_float_with_tolerance(1, 6, 0.01, 20)
+        # print(f'{test} nums: {nums}')
+        max_found = max(max_found, nums.max())
+        min_found = min(min_found, nums.min())
+        filtered_max_values = nums[(nums >= 5.999) & (nums <= 6.001)]
+        filtered_min_values = nums[(nums >= 0.999) & (nums <= 1.001)]
+        # print(f'filtered_values: {filtered_values}')
+        if len(filtered_max_values) > 0:
+            print(f'max_found: {max_found} in test {test}')
+
+        if len(filtered_min_values) > 0:
+            print(f'min_found: {min_found} in test {test}')
+
+
+    print(f'min_found: {min_found}, max_found: {max_found}')
+
+def main():
+    # verify_random_int_works()
+    verify_random_float_works()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
--- random_number_utilities.py
+++ random_number_utilities.py
@@ -20,4 +20,17 @@ class RandomNumberUtilities:
 
         if tolerance < 0.0 or tolerance is None:
             tolerance = (max_value - min_value) / 10_000
-        return np.random.Generator.floating(min_value, max_value + 1, dtype = np.float32, size = size)
+        extended_min_value = min_value - tolerance
+        extended_max_value = max_value + tolerance
+        extended_range = extended_max_value - extended_min_value
+        nums = np.array([], dtype = np.float64)
+        for i in range(size):
+            num = RandomNumberUtilities.rng.random(size = 1) * extended_range + extended_min_value
+            if num < min_value:
+                num = min_value
+            elif num > max_value:
+                num = max_value
+
+            nums = np.append(nums, np.float64(num))
+
+        return nums

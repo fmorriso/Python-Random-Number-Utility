764f4df84878d2e98938b44655a6faf0030d7e4b
Fred Morrison <frederick.morrison@outlook.com>
2025-04-20 11:49:19 -0600

    adjust verification tests

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from random_number_utilities import RandomNumberUtilities
 
 
-def verify_random_int_works(num_tests: int = 100):
+def verify_random_int_works(num_tests: int = 100, test_size: int = 20):
     min_value: int = 1
     max_value: int = 6
 
@@ -14,7 +14,7 @@ def verify_random_int_works(num_tests: int = 100):
     frequency = {'min': 0, 'max': 0}
 
     for test in range(num_tests):
-        nums = RandomNumberUtilities.random_int(min_value, max_value, 20)
+        nums = RandomNumberUtilities.random_int(min_value, max_value, test_size)
         # print(f'nums: {nums}')
 
         max_found = max(max_found, nums.max())
@@ -29,19 +29,20 @@ def verify_random_int_works(num_tests: int = 100):
             frequency['max'] += len(max_matches)
 
     print(f'min_found: {min_found}, max_found: {max_found}')
-    print(f'{num_tests = }, {frequency = }')
+    print(f'{num_tests = }, {test_size = }, {frequency = }')
 
 
-def verify_random_float_with_tolerance_works(num_tests: int = 1_000):
+def verify_random_float_with_tolerance_works(num_tests: int = 1_000, test_size: int = 20):
     min_value: float = 1.0
     max_value: float = 6.0
+    tolerance: float = 0.3
 
     max_found: float = 0
     min_found: float = sys.float_info.max
     # keep track of how many times the min_value and max_value were generated
-    frequency =   {'min': 0, 'max': 0}
+    frequency = {'min': 0, 'max': 0}
     for test in range(num_tests):
-        nums = RandomNumberUtilities.random_float_with_tolerance(min_value, max_value, 0.01, 20)
+        nums = RandomNumberUtilities.random_float_with_tolerance(min_value, max_value, tolerance, test_size)
         # print(f'{test} nums: {nums}')
         max_found = max(max_found, nums.max())
         min_found = min(min_found, nums.min())
@@ -54,11 +55,11 @@ def verify_random_float_with_tolerance_works(num_tests: int = 1_000):
         if len(max_matches) > 0:
             frequency['max'] += len(max_matches)
 
-
     print(f'min_found: {min_found}, max_found: {max_found}')
-    print(f'{num_tests = }, {frequency = }')
+    print(f'{num_tests = }, {test_size = }, {frequency = }')
 
-def verify_random_float_works(num_tests: int = 100):
+
+def verify_random_float_works(num_tests: int = 1_000, test_size: int = 20):
     min_value: float = 1.0
     max_value: float = 6.0
 
@@ -69,7 +70,7 @@ def verify_random_float_works(num_tests: int = 100):
     frequency = {'min': 0, 'max': 0}
 
     for test in range(1_000):
-        nums = RandomNumberUtilities.random_float(min_value, max_value, 20)
+        nums = RandomNumberUtilities.random_float(min_value, max_value, test_size)
 
         # print(f'{test} nums: {nums}')
         max_found = max(max_found, nums.max())
@@ -84,15 +85,15 @@ def verify_random_float_works(num_tests: int = 100):
             frequency['max'] += len(max_matches)
 
     print(f'min_found: {min_found}, max_found: {max_found}')
-    print(f'{num_tests = }, {frequency = }')
+    print(f'{num_tests = }, {test_size = }, {frequency = }')
 
 
 def main():
-    # verify_random_int_works()
+    verify_random_int_works()
     verify_random_float_works()
-    # verify_random_float_with_tolerance_works()
+    verify_random_float_with_tolerance_works()
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
--- random_number_utilities.py
+++ random_number_utilities.py
@@ -1,9 +1,9 @@
-from typing import Any
-
 import numpy as np
+
 
 class RandomNumberUtilities:
     rng = np.random.default_rng()
+
 
     @staticmethod
     def random_int(min_value: int, max_value: int, size: int = 1):
@@ -16,8 +16,9 @@ class RandomNumberUtilities:
         # print(f'tolerance: {tolerance}')
         return RandomNumberUtilities.random_float_with_tolerance(min_value, max_value, tolerance, size)
 
+
     @staticmethod
-    def random_float_with_tolerance(min_value: float, max_value: float, tolerance:float = 0.01, size: int = 1):
+    def random_float_with_tolerance(min_value: float, max_value: float, tolerance: float = 0.01, size: int = 1):
         if tolerance < 0.0 or tolerance is None:
             tolerance = (max_value - min_value) / 10_000
         # print(f'tolerance: {tolerance}')
